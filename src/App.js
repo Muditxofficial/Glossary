a65d2c249d52d038b9ff2629df407fed1f98e7a2
Muditxofficial <muditchoudhary4@gmail.com>
2021-02-07 23:35:06 +0530

    practice

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
+import {useState} from 'react';
 import './App.css';
 
 function App() {
+  const [add,setAdd] = useState([
+    {
+      name:"Gmail",
+      password:"1232"
+    },
+    {
+      name:"Github",
+      password:"1232"
+    },
+    {
+      name:"Facebook",
+      password:"1232"
+    },
+    {
+      name:"LinkedIn",
+      password:"1232"
+    },
+  ])
+  const [iden,setIden] = useState('')
+  const [pass,setPass] = useState('')
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newCred = {
+      name:iden,
+      password:pass
+    }
+    setAdd([...add,newCred])
+    setIden('')
+    setPass('')
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>
+       Password Manager
+     </h1>
+     <h3>Enter new Credentials:</h3>
+     <form onSubmit={handleSubmit}>
+     <div>
+      Identifier:   
+     <input value={iden} type="text" placeholder="enter Identifier" onChange={(e) => setIden(e.target.value)}/>
+     </div>
+     <div>
+       Password:
+     <input value={pass} type="password" placeholder="enter Password" onChange={(e) => setPass(e.target.value)} required/>
+     </div>
+     <button type="submit" >Submit</button>
+     </form>
+     {add.map((x,idx) => (
+       <div key={idx}>
+         {x.name}:{x.password}
+       </div>
+     ))}
+     <h3>Enter Credentials to search:</h3>
+     <input type="text" />
     </div>
   );
 }
+
 
 export default App;
