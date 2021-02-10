25e5976fff3e31b6071c37c24cd599113c5173f0
Muditxofficial <muditchoudhary4@gmail.com>
2021-02-10 15:51:39 +0530

    delete

--- src/Components/Cred.js
+++ src/Components/Cred.js
@@ -1,15 +1,26 @@
 import React from 'react'
 import useFirestore from '../hooks/useFirestore'
+import {db} from '../firebase'
 const Cred = () => {
     const {docs} = useFirestore('users')
     console.log(docs)
+    const handleDeletion = (name) => {
+        const mine = `${name}`
+        db.collection('users').doc(mine).delete().then(() => {
+            console.log("document deleted successfully");
+        }).catch((err) => {
+            console.log("error:",err)
+        })
+    }
     return (
         <div>
             {docs && docs.map(doc => (
                 <div key={doc.id}>
-                    {doc.name}:{doc.password}
+                    <h4>{doc.name}:{doc.password}</h4>
+                    <button onClick={() => handleDeletion(doc.id)}>Delete</button>
                 </div>
             ))}
+            
         </div>
     )
 }
